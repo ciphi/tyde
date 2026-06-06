4d4e128ae75296ac32b256571839da68e503d0ae
Jame Saephanh <19793385+ciphi@users.noreply.github.com>
2026-06-05 22:22:56 -0700

    refactor: fix bootstrap ordering in map

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 mod cli;
 mod config;
+mod db;
 mod logging;
+
 use config::Config;
+use db::library::Library;
 use logging::AppLog;
 
 pub mod utils;
 
 fn main() {
-    let cli = cli::get();
-    utils::verbose::set_verbose(cli.verbose);
-    let _app_log = AppLog::default().init();
+    let _log = AppLog::default().init();
+
     let cfg = Config::new();
     let data = cfg.load_or_create();
+
+    let _db = Library::new(data.library.db_path.clone());
+
+    let cli = cli::get();
+    utils::verbose::set_verbose(cli.verbose);
 }
