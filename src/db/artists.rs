353a00bdc80473cfde9777b9e22e238a55e92d07
Jame Saephanh <19793385+ciphi@users.noreply.github.com>
2026-06-06 18:55:55 -0700

    feat: db insert

--- src/db/artists.rs
+++ src/db/artists.rs
@@ -9,3 +9,18 @@ struct Artist {
     created_at: DateTime<Utc>,
     updated_at: DateTime<Utc>,
 }
+
+pub fn insert(conn: &Connection, name: &str) -> Result<Artist> {
+    let now = Utc::now();
+    conn.execute(
+        "INSERT INTO artists (name, created_at, updated_at) VALUES (?1, ?2, ?3)",
+        params![name, now, now],
+    )?;
+    let id = conn.last_insert_rowid();
+    Ok(Artist {
+        id,
+        name: name.to_string(),
+        created_at: now,
+        updated_at: now,
+    })
+}
