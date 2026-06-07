b949ea42d25a6154a518f6d26d630a20f9c34d2e
Jame Saephanh <19793385+ciphi@users.noreply.github.com>
2026-06-06 20:49:02 -0700

    refactor: handle errors in main

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ mod db;
 mod logging;
 pub mod utils;
 
+use std::process::exit;
+
 use config::Config;
 use db::library::Library;
 use logging::AppLog;
+use tracing::error;
 
 fn main() {
     let _log = AppLog::default().init();
@@ -14,8 +17,16 @@ fn main() {
     let cfg = Config::new();
     let data = cfg.load_or_create();
 
-    let _db = Library::init(data.library.db_path.clone());
+    let Ok(db) = Library::init(data.library.db_path.clone()) else {
+        error!("Failed to initialize library");
+        exit(1);
+    };
 
-    let cli = cli::get();
+    let Ok(cli) = cli::get(&db).map_err(|e| {
+        error!("Failed to initialize cli");
+        error!("{:?}", e);
+        exit(1);
+    });
+
     utils::verbose::set_verbose(cli.verbose);
 }
