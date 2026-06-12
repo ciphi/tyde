9511500852e4068c2f9cfa0129cede504857d9e9
Jame Saephanh <19793385+ciphi@users.noreply.github.com>
2026-06-12 13:10:28 -0700

    tests: artist integration

--- tests/artist_integration.rs
+++ tests/artist_integration.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 use isolang::Language;
 use tyde::{
+    cli::commands::artist::{NameRecord, NameVariantRecord},
     db::library::Library,
     library::artists::{model::NameKind, repository::ArtistRepository},
 };
@@ -19,8 +20,9 @@ fn add_creates_name_and_sets_primary() -> Result<(), Box<dyn std::error::Error>>
     let lib = setup_library();
     let repo = repository(&lib);
 
-    let artist_name = "Test_Artist_Name";
-    let result = repo.add(artist_name, None, None)?;
+    let record = NameRecord::new("Test Artist".into(), None, None);
+    let variant: Vec<NameVariantRecord> = Vec::new();
+    let result = repo.add(&record, &variant)?;
 
     // Assert the row was created in artists table.
     assert_eq!(result, 1);
@@ -41,7 +43,7 @@ fn add_creates_name_and_sets_primary() -> Result<(), Box<dyn std::error::Error>>
         |row| row.get(0),
     )?;
 
-    assert_eq!(artist_name, &name);
+    assert_eq!(&record.name, &name);
 
     Ok(())
 }
@@ -51,10 +53,12 @@ fn add_sets_kind_and_locale() -> Result<(), Box<dyn std::error::Error>> {
     let lib = setup_library();
     let repo = repository(&lib);
 
-    let artist_name = "Test_Artist_Name";
-    let kind = NameKind::Alias;
-    let lang = Language::from_639_1("en");
-    let result = repo.add(artist_name, Some(kind.clone()), lang.clone())?;
+    let locale = Language::from_639_1("en");
+    let kind = Some(NameKind::Alias);
+    let record = NameRecord::new("Test Artist Name".into(), locale, kind);
+
+    let variant: Vec<NameVariantRecord> = Vec::new();
+    let result = repo.add(&record, &variant)?;
 
     // Assert the row was created in artists table.
     assert_eq!(result, 1);
@@ -75,12 +79,56 @@ fn add_sets_kind_and_locale() -> Result<(), Box<dyn std::error::Error>> {
         |row| Ok((row.get(0)?, row.get(1)?)),
     )?;
 
-    assert_eq!(kind.to_string(), row_kind);
+    assert_eq!(record.name_type.unwrap().to_string(), row_kind);
+    assert_eq!(record.locale.unwrap().to_639_1().unwrap(), row_locale);
 
-    let lang = lang.unwrap();
-    let code = lang.to_639_1().unwrap();
+    Ok(())
+}
 
-    assert_eq!(code, row_locale);
+#[test]
+fn add_apply_all_variants() -> Result<(), Box<dyn std::error::Error>> {
+    let lib = setup_library();
+    let repo = repository(&lib);
+
+    let locale = Language::from_639_1("en");
+    let kind = Some(NameKind::Alias);
+    let record = NameRecord::new("Test Artist Name".into(), locale, kind);
+    let mut variants: Vec<NameVariantRecord> = Vec::new();
+
+    for i in 0..5 {
+        let variant_name = format!("Name Variant {i}");
+        let variant_locale = Language::from_639_1("en");
+        let variant_kind = Some(NameKind::Alias);
+        let variant_record = NameRecord::new(variant_name, variant_locale, variant_kind);
+        variants.push(NameVariantRecord::new(variant_record));
+    }
+
+    let result = repo.add(&record, &variants)?;
+
+    // Assert the row was created in artists table.
+    assert_eq!(result, 1);
+
+    // Get the primary ID from the new row
+    let primary_name_id: i64 = lib.conn.query_row(
+        "SELECT primary_name_id FROM artists WHERE id = ? ",
+        [result],
+        |row| row.get(0),
+    )?;
+
+    assert_eq!(primary_name_id, result);
+
+    for i in 0..variants.len() {
+        let variant_name = format!("Name Variant {i}");
+        let (artist_id, row_kind, row_locale): (i64, String, String) = lib.conn.query_row(
+            "SELECT artist_id, kind, locale FROM artist_names WHERE name = ?1",
+            [variant_name],
+            |row| Ok((row.get(0)?, row.get(1)?, row.get(2)?)),
+        )?;
+
+        assert_eq!(result, artist_id);
+        assert_eq!(record.name_type.unwrap().to_string(), row_kind);
+        assert_eq!(record.locale.unwrap().to_639_1().unwrap(), row_locale);
+    }
 
     Ok(())
 }
